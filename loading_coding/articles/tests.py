33b6fc506a2671c6dc71e108ae48ca70b1cb97a6
Kamil Abzalov <iamkabzal@yandex.ru>
2024-06-26 16:51:03 +0300

    added tests

--- loading_coding/articles/tests.py
+++ loading_coding/articles/tests.py
@@ -1,6 +1,58 @@
+from http import HTTPStatus
+
 from django.test import TestCase
+from django.urls import reverse
+
+from articles.models import Posts
+
 
 class GetPostsTestCase(TestCase):
-    fixtures = []
 
-# ---!-----В разработке-----!--- #
+    fixtures = [
+        "all_data.json",
+    ]
+
+    def setUp(self):
+        "Инициализируется перед выполнением каждого теста"
+
+    def test_home_page(self):
+        path = reverse('home')
+        response = self.client.get(path)
+        self.assertEqual(response.status_code, HTTPStatus.OK)
+        self.assertTemplateUsed(response, 'articles/index.html')
+        self.assertEqual(response.context_data['title'], "Все статьи")
+
+    def test_data_home_page(self):
+        posts = Posts.published.all().select_related('cat').order_by('-time_update')
+        path = reverse('home')
+        response = self.client.get(path)
+        self.assertQuerySetEqual(response.context_data['posts'], posts[:3])
+
+    def test_redirect(self):
+        path = reverse('add_post')
+        redirect_uri = reverse('users:login') + '?next=' + path
+        response = self.client.get(path)
+        self.assertEqual(response.status_code, HTTPStatus.FOUND)
+        self.assertRedirects(response, redirect_uri)
+
+    def test_paginate_homepage(self):
+        path = reverse('home')
+        page = 2
+        paginate_by = 3
+        response = self.client.get(path + f"?page={page}")
+        posts = Posts.published.all().select_related('cat').order_by('-time_update')
+        print(posts[(page - 1) * paginate_by:page * paginate_by])
+        print(response.context_data['posts'])
+        self.assertQuerySetEqual(response.context_data['posts'],
+                                 posts[(page-1) * paginate_by:page * paginate_by])
+
+    def test_post_redirect(self):
+        post = Posts.published.get(pk=1)
+        path = reverse('post', args=[post.slug])
+        redirect_uri = reverse('users:login') + '?next=' + path
+        response = self.client.get(path)
+        self.assertEqual(response.status_code, HTTPStatus.FOUND)
+        self.assertRedirects(response, redirect_uri)
+
+    def tearDown(self):
+        "Действия после выполняния каждого теста"
--- loading_coding/users/tests.py
+++ loading_coding/users/tests.py
@@ -1,3 +1,51 @@
-from django.test import TestCase
+from http import HTTPStatus
 
-# Create your tests here.
+from django.contrib.auth import get_user_model
+from django.test import TestCase
+from django.urls import reverse
+
+
+class RegisterUserTestCase(TestCase):
+    fixtures = ["all_data.json"]
+
+    def setUp(self):
+        self.data = {
+            'username': 'useless_user',
+            'email': 'useless@mail.com',
+            'first_name': 'Бесполезник',
+            'last_name': 'Бесполезный',
+            'password1': 'Useless1password2',
+            'password2': 'Useless1password2',
+        }
+
+    def test_form_registration_get(self):
+        path = reverse('users:register')
+        response = self.client.get(path)
+        self.assertEqual(response.status_code, HTTPStatus.OK)
+        self.assertTemplateUsed(response, 'users/register.html')
+
+    def test_user_registration_success(self):
+        user_model = get_user_model()
+        path = reverse('users:register')
+        response = self.client.post(path, self.data)
+
+        self.assertEqual(response.status_code, HTTPStatus.FOUND)
+        self.assertRedirects(response, reverse('users:login'))
+        self.assertTrue(user_model.objects.filter(username=self.data['username']).exists())
+
+    def test_user_registration_password_error(self):
+        self.data['password2'] = 'Useful1password2'
+        path = reverse('users:register')
+        response = self.client.post(path, self.data)
+
+        self.assertEqual(response.status_code, HTTPStatus.OK)
+        self.assertContains(response, "Введенные пароли не совпадают.", html=True)
+
+    def test_user_registration_exists_error(self):
+        user_model = get_user_model()
+        user_model.objects.create(username=self.data['username'])
+
+        path = reverse('users:register')
+        response = self.client.post(path, self.data)
+        self.assertEqual(response.status_code, HTTPStatus.OK)
+        self.assertContains(response, "Пользователь с таким именем уже существует")
